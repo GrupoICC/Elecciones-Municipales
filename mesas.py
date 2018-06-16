c0f7811e1d6b81375dfe821fac6351bcee7ac731
MaryCarmenPariManuel <39710785+MaryCarmenPariManuel@users.noreply.github.com>
2018-06-16 00:13:03 -0500

    Update mesas.py

--- mesas.py
+++ mesas.py
@@ -1,1 +1,10 @@
-
+def NumeroMesas():
+    munivotantes=[]
+    muni = input('Ingrese el nombre de la municipalidad: ')
+    munimesas={muni:munivotantes}
+    mesas = int(input('Ingrese el número de mesas: '))
+    for i in range(mesas):
+        votantes = int(input('Ingrese el número de votantes de la mesa '+str(i+1)+': '))
+        munivotantes.append(votantes)
+    return munimesas
+NumeroMesas()
